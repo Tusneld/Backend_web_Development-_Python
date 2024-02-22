706fdff041e1571a1992362e76a245c6eb9c8566
Tusnelde Endjala <tusneldelainae@gmail.com>
2024-02-23 00:52:03 +0200

    This is my Assignment 1 for Python Backend

---
+++ index.py
@@ -0,0 +1,18 @@
+name = "Laina"
+school = "Blizzcode Academy"
+class_name = "Backend Web Development with Python"
+
+print("Personal Details:")
+print("Name:", name)
+print("School:", school)
+print("Class:", class_name)
+print()
+
+empty_list = []
+print("Empty List:", empty_list)
+
+empty_list.append("Subject 1") 
+empty_list.append("Subject 2") 
+empty_list.append("Subject 3")
+
+print("Updated List:", empty_list)  
